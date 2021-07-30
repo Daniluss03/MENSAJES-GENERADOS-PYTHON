9e35c9bc22c2ecf58a1009a3d9c7b487737190e5
Daniluss03 <87955581+Daniluss03@users.noreply.github.com>
2021-07-30 12:26:42 -0500

    Update mensajes.py

--- mensajes.py
+++ mensajes.py
@@ -2,7 +2,7 @@
 
 import pyautogui as pg, webbrowser as web,time as tn
 
-web.open('https://web.whatsapp.com/send?phone=+573229005417')
+web.open('https://web.whatsapp.com/send?phone= put the phone number here ')
 tn.sleep(5)
 #this is the range ofe messages that we are gonna send 
 for i in range (300):
