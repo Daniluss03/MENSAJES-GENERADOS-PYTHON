0f694fe5aa04311acf833b40863271f0e9d05285
Daniluss03 <87955581+Daniluss03@users.noreply.github.com>
2021-07-30 12:25:04 -0500

    Create mensajes.py

---
+++ mensajes.py
@@ -0,0 +1,11 @@
+# try this code with sublime text
+
+import pyautogui as pg, webbrowser as web,time as tn
+
+web.open('https://web.whatsapp.com/send?phone=+573229005417')
+tn.sleep(5)
+#this is the range ofe messages that we are gonna send 
+for i in range (300):
+    pg.write("Acabas de entrar en un bucle daniluss")
+    pg.press('enter')
+    pass
